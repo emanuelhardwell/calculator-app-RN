3156522af442345878a08d89422992fe0bcbeec3
emanuelhardwell <emanuelnoe16@gmail.com>
2026-01-09 23:14:15 -0600

    feat: Create component CalculatorButton

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
+import { CalculatorButton } from "@/components/CalculatorButton";
 import { TextCustom } from "@/components/TextCustom";
+import { Colors } from "@/constants/Colors";
 import { globalStyles } from "@/styles/global-styles";
 import React from "react";
 import { View } from "react-native";
@@ -6,8 +8,36 @@ import { View } from "react-native";
 const CalculatorApp = () => {
   return (
     <View style={globalStyles.calculatorContainer}>
-      <TextCustom variant="h1">10 x 1,000,000,000</TextCustom>
-      <TextCustom variant="h2">1,000,000,000</TextCustom>
+      <View>
+        <TextCustom variant="h1">10 x 1,000,000,000</TextCustom>
+        <TextCustom variant="h2">1,000,000,000</TextCustom>
+      </View>
+
+      <View style={globalStyles.row}>
+        <CalculatorButton
+          label="C"
+          textBlack
+          color={Colors.lightGray}
+          onPress={() => {}}
+        ></CalculatorButton>
+        <CalculatorButton
+          label="+/-"
+          textBlack
+          color={Colors.lightGray}
+          onPress={() => {}}
+        ></CalculatorButton>
+        <CalculatorButton
+          label="del"
+          textBlack
+          color={Colors.lightGray}
+          onPress={() => {}}
+        ></CalculatorButton>
+        <CalculatorButton
+          label="÷"
+          color={Colors.orange}
+          onPress={() => {}}
+        ></CalculatorButton>
+      </View>
     </View>
   );
 };
